dd6c3731b2a7d90f15b84afbeffe4add59642210
Ozzzzzi <48116812+Ozzzzzi@users.noreply.github.com>
2019-11-29 03:45:12 +0700

    Update TheShortestPath.java

--- TASK_1_2/src/main/java/TheShortestPath.java
+++ TASK_1_2/src/main/java/TheShortestPath.java
@@ -1,5 +1,18 @@
 public class TheShortestPath {
-
+    /**
+     * Finds the shortest path between <code>startV</code> and <code>endV</code> vertices
+     * in the weighted graph.
+     * The graph is given by the adjacency matrix <code>adjMtx</code>. It cannot contain edges 
+     * with negative weights and loops. The absence of an edge between vertices is -1 value. 
+     * Vertex numbering starts with 1.
+     * 
+     * @param  vNum    number of vertices in the graph
+     * @param  adjMtx  adjacency matrix of the graph
+     * @param  startV  number of the start vertex 
+     * @param  endV    number of the end vertex
+     * @return         the shortest distance between <code>startV</code> and <code>endV</code>
+     *                 or -1 if there is no path
+     */
     public int findPath(int vNum, int[][] adjMtx, int startV, int endV) {
         int INF = Integer.MAX_VALUE / 2;
         int[][] dist = new int[vNum][vNum];
