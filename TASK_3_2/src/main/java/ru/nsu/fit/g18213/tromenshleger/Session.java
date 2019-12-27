ac8111846d50c2541fc3eac98af619a21a6b24aa
Ozzzzzi <48116812+Ozzzzzi@users.noreply.github.com>
2019-12-27 12:14:06 +0700

    Update Session.java

--- TASK_3_2/src/main/java/ru/nsu/fit/g18213/tromenshleger/Session.java
+++ TASK_3_2/src/main/java/ru/nsu/fit/g18213/tromenshleger/Session.java
@@ -7,31 +7,62 @@ public class Session {
     private ArrayList<Exam> exams;
     private ArrayList<DiffCredit> diffCredits;
     private ArrayList<Credit> credits;
-
+    
+/**
+ * Constructor which just initializes the fields.
+ */
     public Session() {
         this.exams = new ArrayList<>();
         this.diffCredits = new ArrayList<>();
         this.credits = new ArrayList<>();
     }
 
+/**
+ * Adds an exam to the exams list.
+ *
+ * @param   name        the exam title
+ * @param   grade       the exam grade from 2 to 5
+ */
     public void addExam(String name, int grade) {
         exams.add(new Exam(name, grade));
     }
 
+/**
+ * Adds a diff credit to the diff credits list.
+ *
+ * @param   name        the diff credit title
+ * @param   grade       the credit grade from 2 to 5
+ */
     public void addDiffCredit(String name, int grade) {
         diffCredits.add(new DiffCredit(name, grade));
     }
-
+    
+/**
+ * Adds a credit to the credits list.
+ *
+ * @param   name        the credit title
+ * @param   grade       the credit grade "pass" and "notpass"
+ */
     public void addCredit(String name, String grade) {
         credits.add(new Credit(name, grade));
     }
-
+    
+/**
+ * Removes the <code>name</code> subject from the session.
+ *
+ * @param   name        the credit title
+ */
     public void removeSubject(String name) {
         exams.removeIf(subj -> subj.getName().equals(name));
         diffCredits.removeIf(subj -> subj.getName().equals(name));
         credits.removeIf(subj -> subj.getName().equals(name));
     }
 
+/**
+ * Calculates the average grade for this session.
+ *
+ * @return    the average grade
+ */
     public double getAverageGrade() {
         int count = getGradesNum();
         if (count == 0) {
@@ -41,6 +72,11 @@ public class Session {
         }
         }
 
+/**
+ * Checks whether there are any "notpass" credits in this session.
+ *
+ * @return    true if there is no "notpass" and false otherwise
+ */
     public boolean noNotPass() {
         for (Credit cred: credits) {
             if (cred.getGrade().equals("notpass")) {
@@ -50,6 +86,11 @@ public class Session {
         return true;
     }
 
+/**
+ * Gets the sum of all grades for exams and diff credits for further usage.
+ *
+ * @return    the sum of the grades
+ */
     public double getGradesSum() {
         double sum = 0;
         for (Exam ex : exams) {
@@ -60,7 +101,11 @@ public class Session {
         }
         return sum;
     }
-
+/**
+ * Gets the total number exams and diff credits for further usage.
+ *
+ * @return    the total number of exams and diff credits
+ */
     public int getGradesNum() {
         return exams.size() + diffCredits.size();
     }
