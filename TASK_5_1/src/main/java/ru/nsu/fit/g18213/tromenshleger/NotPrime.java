965a0ea66215e8cc9585ec2e1b91ed27c3d99492
Ozzzzzi <48116812+Ozzzzzi@users.noreply.github.com>
2019-12-27 11:20:46 +0700

    Update NotPrime.java

--- TASK_5_1/src/main/java/ru/nsu/fit/g18213/tromenshleger/NotPrime.java
+++ TASK_5_1/src/main/java/ru/nsu/fit/g18213/tromenshleger/NotPrime.java
@@ -1,7 +1,14 @@
 package ru.nsu.fit.g18213.tromenshleger;
 
+
 public class NotPrime {
 
+/**
+ * Checks whether <code>n</code> is not prime number.
+ * @param   n   the number to check
+ * @return      true if the number is not prime
+ *              and false otherwise
+ */
     static boolean check(int n) {
         if (n < 2) {
             return true;
